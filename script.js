31e381242bee826c7874688509ba23a96f7011bd
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-03-01 14:52:04 +0100

    create function boxClicked, function restart, eventlisteners to btn and boxes

--- script.js
+++ script.js
@@ -6,15 +6,35 @@ let boxes = Array.from(document.getElementsByClassName('box')); //this gives an 
 
 const O_TEXT = "O";
 const X_TEXT = "X";
-let currentPlayer = X_TEXT;
+let currentPlayer = X_TEXT;//defaultplayer
 let spaces = Array(9).fill(null);
 
+//Eventlistener to each of our 9 boxes
 const startGame = () => {
     boxes.forEach(box => box.addEventListener('click', boxClicked))
 }
 
 function boxClicked(e) {
-    console.log(e.target)
+    const id = e.target.id;
+
+    if(!spaces[id]){
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
+    }
+}
+
+restartBtn.addEventListener('click', restart)
+
+function restart() {
+    spaces.fill(null)
+
+    boxes.forEach(box => {
+        box.innerText = '';
+    })
+
+    currentPlayer = X_TEXT; //back to defaultplayer
 }
 
 startGame();
